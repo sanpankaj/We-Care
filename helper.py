00a51b4b817b5e1fedb2b9bd83244e61762b6fbb
sanpankaj <pankajmaurya4041@gmail.com>
2024-03-07 13:55:22 +0530

    keywords added

--- helper.py
+++ helper.py
@@ -37,7 +37,7 @@ def most_busy_users(df):
 
 def create_wordcloud(selected_user,df):
 
-    f = open('stop_hinglish.txt', 'r')
+    f = open('keywords.txt', 'r')
     stop_words = f.read()
 
     if selected_user != 'Overall':
@@ -60,7 +60,7 @@ def create_wordcloud(selected_user,df):
 
 def most_common_words(selected_user,df):
 
-    f = open('stop_hinglish.txt','r')
+    f = open('keywords.txt','r')
     stop_words = f.read()
 
     if selected_user != 'Overall':
